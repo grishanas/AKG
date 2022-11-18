9bbbe0274c9ecac2c6a680492d3ebe5f84f1113a
grishanas <80056655+grishanas@users.noreply.github.com>
2022-11-18 13:39:15 +0300

    up

--- Camera.cpp
+++ Camera.cpp
@@ -32,7 +32,15 @@ void Camera::SetDecart()
 	this->_ChangeCameraView();
 }
 
-Camera::Camera() {
+Camera::Camera() 
+{
+	this->_radius = 2;
+	this->Parametr.height=800;
+	this->Parametr.width=800;
+	this->Parametr.z_near = 100;
+	this->Parametr.z_far = 50;
+	_target = sf::Vector3f(0, 0, 0);
+	this->SetDecart();
 
 }
 
@@ -50,62 +58,83 @@ sf::Vector3f Normilize(sf::Vector3f NonNormVector)
 
 sf::Vector3f Cross(sf::Vector3f a, sf::Vector3f b)
 {
-	sf::Vector3f newVect = sf::Vector3f(a.y*b.z-a.z*b.y,a.x*b.z-a.z*b.x,a.x*b.y+a.z*b.y );
+	sf::Vector3f newVect = sf::Vector3f(a.y*b.z-a.z*b.y,a.z*b.x-a.x*b.z,a.x*b.y-a.y*b.x );
 	return newVect;
 }
 
 void Camera::_ChangeCameraView() 
 {
-	this->_direction = Normilize(this->_decartCoords - sf::Vector3f(.0f,.0f,.0f));
-	this->_cameraRight = Normilize(Cross(sf::Vector3f(.0f, 1.f, .0f), this->_direction));
+
+	this->_direction = Normilize(this->_decartCoords - this->_target);
+	this->_cameraRight = Normilize(Cross(Cross(sf::Vector3f(.0f, 1.f, .0f), this->_direction), this->_direction));
 	this->_cameraUp = Normilize(Cross(this->_direction, this->_cameraRight));
 }
 
-void Camera::Render(Model* model)
+float DotProduct(sf::Vector3f a, sf::Vector3f b)
 {
-	for (int i = 0; i < model->nverts(); i++)
-	{
-		auto vec = model->vert(i);
-		sf::Vector3f tmp;
-
-		for (int k = 0; k < 3; k++)
-		{
-			//tmp.x = vec.x * this->_rotation.x+ vec.x * this->_Rotation.y+ vec.x * this->_Rotation.z;
-			//tmp.y = vec.y * this->_rotation.x+ vec.y * this->_Rotation.y+ vec.y * this->_Rotation.z;
-			//tmp.z = vec.z * this->_rotation.x+ vec.z * this->_rotation.y+ vec.z * this->_Rotation.z;
-		}
-		this->_direction = Normilize(this->_decartCoords - this->_Target);
-		sf::Vector3f tmp = Cross(sf::Vector3f(.0f, 1.f, .0f), this->_direction);
-		this->_cameraRight = Normilize(tmp);
-		this->_cameraUp = Normilize(Cross(this->_direction, this->_CameraRight));
-	}
-
+	return a.x * b.x + a.y * b.y + a.z * b.z;
 }
+
+sf::Vector3f Camera::_CameraView(sf::Vector3f point)
 
 void Camera::Render(Model* model)
 {
-	for (int i = 0; i < model->nverts(); i++)
-	{
-		auto vec = model->vert(i);
-		sf::Vector3f tmp;
-
-		for (int k = 0; k < 3; k++)
-		{
-			tmp.x = vec.x * this->_Rotation.x+ vec.x * this->_Rotation.y+ vec.x * this->_Rotation.z;
-			tmp.y = vec.y * this->_Rotation.x+ vec.y * this->_Rotation.y+ vec.y * this->_Rotation.z;
-			tmp.z = vec.z * this->_Rotation.x+ vec.z * this->_Rotation.y+ vec.z * this->_Rotation.z;
-		}
-	}
+	sf::Vector3f newPoint;
+	newPoint.x = point.x * this->_cameraRight.x + point.y * this->_cameraRight.y + point.z * this->_cameraRight.z- DotProduct(this->_cameraRight, this->_decartCoords);
+	newPoint.y = point.x * this->_cameraUp.x + point.y * this->_cameraUp.y + point.z * this->_cameraUp.z- DotProduct(this->_cameraUp, this->_decartCoords);
+	newPoint.z = point.x * this->_direction.x + point.y * this->_direction.y + point.z * this->_direction.z- DotProduct(this->_direction, this->_decartCoords);
+	return newPoint;
 }
-//sf::Vector3f Camera::Rende(sf::Vector3f a)
-//{
-//
+
+sf::Vector3f Camera::MatrixProjection(sf::Vector3f point)
+{
+	sf::Vector3f newPoint;
+	newPoint.x = point.x * 2 / this->Parametr.width;
+	newPoint.y = point.y * 2 / this->Parametr.height;
+	newPoint.z = point.z / (this->Parametr.z_near - this->Parametr.z_far) + point.z * this->Parametr.z_near / (this->Parametr.z_near - this->Parametr.z_far);
+	return newPoint;
+}
+
+sf::Vector3f  Camera::MatrixPerspective(sf::Vector3f point)
+{
+	sf::Vector3f newPoint;
+	float w = 1; //point.x* point.y* point.z;
+
+	newPoint.x = point.x * 2*this->Parametr.z_near / this->Parametr.width/w;
+	newPoint.y = point.y * 2 * this->Parametr.z_near / this->Parametr.height/w;
+	newPoint.z = (point.z*(this->Parametr.z_far + this->Parametr.z_near*this->Parametr.z_far)/ (this->Parametr.z_near - this->Parametr.z_far) )/w;
+	return newPoint;
+}
+
+sf::Vector3f Camera::MatrixviewPort(sf::Vector3f point)
+{
+	sf::Vector3f newPoint;
+
+	newPoint.x = point.x * (this->Parametr.width+1) ;
+	newPoint.y = point.y * ( - this->Parametr.height / 2 - this->Parametr.height / 2);
+	newPoint.z = point.z;
+	return newPoint;
+		}
+
+sf::Vector3i Camera::Render(sf::Vector3f point)
+{
+	sf::Vector3f temp;
+
+	temp = this->_CameraView(point);
+	temp = this->MatrixPerspective(temp);
+	temp = this->MatrixProjection(temp);
+	temp = this->MatrixviewPort(temp);
+
+
+	sf::Vector3i newPoint = sf::Vector3i((temp.x+1.f)*this->Parametr.width/2, (temp.y+1.f) * this->Parametr.height / 2, temp.z);
+	return newPoint;
+}
 //
 //}
 
 void Camera::_SphereToDecart()
 {
-	this->_decartCoords.x = this->_radius * sin(_angles.y) * cos(_angles.x);
-	this->_decartCoords.y = this->_radius * sin(_angles.y) * sin(_angles.x);
-	this->_decartCoords.z = this->_radius * cos(_angles.y);
+	this->_decartCoords.x = this->_radius * sin(1-_angles.y) * cos(_angles.x);
+	this->_decartCoords.y = this->_radius * sin(1-_angles.y) * sin(_angles.x);
+	this->_decartCoords.z = this->_radius * cos(1-_angles.y);
 }
